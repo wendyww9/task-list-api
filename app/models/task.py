f98f2cbee28d92f01b56c4683348771bd487f864
wendyww9 <wendyskystar@gmail.com>
2025-05-08 16:44:39 -0500

    fix goal_id

--- app/models/task.py
+++ app/models/task.py
@@ -21,7 +21,8 @@ class Task(db.Model):
         task_as_dict["title"] = self.title
         task_as_dict["description"] = self.description
         task_as_dict["is_complete"] = self.is_complete
-        task_as_dict["goal_id"] = self.goal_id
+        if self.goal_id:
+            task_as_dict["goal_id"] = self.goal_id
         return task_as_dict
     
     @classmethod
